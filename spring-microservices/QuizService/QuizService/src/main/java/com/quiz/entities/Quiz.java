37002f4a74e05d5b636b6c56bca0dfbbee5fe085
anubhav0908 <155910565+anubhav0908@users.noreply.github.com>
2025-03-20 17:00:37 +0530

    [Anubhav]Added. basic microservices setup with Eureka and basic service configurations

---
+++ spring-microservices/QuizService/QuizService/src/main/java/com/quiz/entities/Quiz.java
@@ -0,0 +1,30 @@
+package com.quiz.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    transient private List<Question> questions ;
+}
